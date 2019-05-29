f4418fe351ae19bd2564d149d1d81786bddcee28
LegrandT <43850904+LegrandT@users.noreply.github.com>
2019-05-29 03:20:19 +0200

    Work in progress shape of input for classifier

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ for s,p,o in g:
 
 print(model.wv.syn0)
 
+
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 #
 # clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X_train, y_train)
